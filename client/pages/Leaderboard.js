e2d201c2a73ef5a423c74f26a4f27ca8b34a19b1
Carla Bernardino <carla.matos.bernardino@celfocus.com>
2016-09-10 14:16:24 +0100

    new leaderboard tags new icon leaderboard

--- client/pages/Leaderboard.js
+++ client/pages/Leaderboard.js
@@ -32,7 +32,87 @@ Template.Leaderboard.helpers({
 
 		return userScores;
 	},
-
+	userHealthScores() {
+		
+		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}}); 
+		var users = latestAcademy.users;
+		
+		var total_points = 0;
+		
+		$.each(users, function(index_users, value_users){
+			
+			var user_points = 0;
+			
+			$.each(value_users.score, function(index_score, value_score){
+				
+				if(value_score.category=="HP"){
+					user_points += value_score.points;
+				}
+				
+			});
+			
+			value_users.totalHealthScore = user_points;
+			total_points += user_points;
+		});
+		
+		sortArrOfObjectsByParam(users, "totalHealthScore", false);
+	
+		return users;
+	},
+	userKnowledgeScores() {
+		
+		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}}); 
+		var users = latestAcademy.users;
+		
+		var total_points = 0;
+		
+		$.each(users, function(index_users, value_users){
+			
+			var user_points = 0;
+			
+			$.each(value_users.score, function(index_score, value_score){
+				
+				if(value_score.category=="KP"){
+					user_points += value_score.points;
+				}
+				
+			});
+			
+			value_users.totalKnowledgeScore = user_points;
+			total_points += user_points;
+		});
+		
+		sortArrOfObjectsByParam(users, "totalKnowledgeScore", false);
+	
+		return users;
+	},
+	userExperienceScores() {
+		
+		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}}); 
+		var users = latestAcademy.users;
+		
+		var total_points = 0;
+		
+		$.each(users, function(index_users, value_users){
+			
+			var user_points = 0;
+			
+			$.each(value_users.score, function(index_score, value_score){
+				
+				if(value_score.category=="XP"){
+					user_points += value_score.points;
+				}
+				
+			});
+			
+			value_users.totalExperienceScore = user_points;
+			total_points += user_points;
+		});
+		
+		sortArrOfObjectsByParam(users, "totalExperienceScore", false);
+	
+		return users;
+	}
 });
 
 
