d8d4db75fb40f0c6c74a24aa7558000cca47d336
Monica Abreu <monica.henriques.abreu@celfocus.com>
2016-09-10 03:07:16 +0100

    leaderboard basic

--- client/partials/Leaderboard_dash.js
+++ client/partials/Leaderboard_dash.js
@@ -1,33 +1,16 @@
-var users = [{
-	nb: "nb22477",
-	name:"Nuno Silva",
-	avatar: "nb22477.jpg",
-	score:[{
-			date: new Date(),
-			challenge: "kahoot",
-			category: "HP",
-			points: 20
-		}],
-	profile: "trainee",
-	skills:[{
-			people: 8,
-			communication: 6,
-			manegemment:7,
-			problemSolving: 7,
-			android:2,
-			arduino:0
-		}]
-}];
+import { Template } from 'meteor/templating';
+import { Academy } from '/imports/api/databasedriver.js';
 			
 
-Template.Leaderboard_dash.onRendered(
-	function() {
+Template.Leaderboard_dash.helpers({
+	users() {
 
-		//debugger;
+		debugger;
 		
-		var total_points = 0;
+		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}}); 
+		var users = latestAcademy.users;
 		
-		//var users [] =//db.academy.find();
+		/*var total_points = 0;	
 		var total_users = users.length;
 		
 		$.each(users, function(index_users, value_users){
@@ -40,11 +23,16 @@ Template.Leaderboard_dash.onRendered(
 				
 			});
 			
-			var tr_ ="<tr><td><img src='" + value.avatar + "' style='width:226px;height:226px;'></td><td>" + value.name + "</td><td>" + user_points + "</td></tr>";
-			console.log( tr_);
+			value_users.totalScore = total_points;
+			
+			//var tr_ ="<tr><td><img src='" + value.avatar + "' style='width:226px;height:226px;'></td><td>" + value.name + "</td><td>" + user_points + "</td></tr>";
+			//console.log( tr_);
 
-			$("#table_leaderboard_dash").append(tr_);
+			//$("#table_leaderboard_dash").append(tr_);
 		});
 		
-		$("#div_pontos_equipa").html(total_points);
+		//$("#div_pontos_equipa").html(total_points);
+		*/
+		return users;
+	}
 });
