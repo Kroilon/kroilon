7863f0f57738530ea6f015cb5a895dd8e7e81cf4
kroilon <bojan@ua.pt>
2016-09-17 12:03:31 +0100

    changed var discovered to type boolean

--- .database/initSecrets.js
+++ .database/initSecrets.js
@@ -16,72 +16,72 @@ db.secrets.insert([
 		description: "Primeiro segredo",
 		nb: "nb21000",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Segundo segredo",
 		nb: "nb21001",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Terceiro segredo",
 		nb: "nb21002",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Quarto segredo",
 		nb: "nb21003",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Quinto segredo",
 		nb: "nb21004",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Sexto segredo",
 		nb: "nb21005",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Sétimo segredo",
 		nb: "nb21001",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Oitavo segredo",
 		nb: "nb21001",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Nono segredo",
 		nb: "nb21003",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Décimo segredo",
 		nb: "nb21004",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Décimo primeiro segredo",
 		nb: "nb21000",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	},
 	{
 		description: "Décimo segundo segredo",
 		nb: "nb21004",
 		challenge: "Submeter Segredo",
-		discovered: 0
+		discovered: false
 	}]
 );
