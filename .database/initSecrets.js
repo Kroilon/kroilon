bf9ed36fe93e2f304c73633881ebd1a1647c8868
kroilon <bojan@ua.pt>
2016-09-23 00:18:30 +0100

    Added name attribute to secrets script

--- .database/initSecrets.js
+++ .database/initSecrets.js
@@ -15,73 +15,97 @@ db.secrets.insert([
 	{
 		description: "Primeiro segredo",
 		nb: "nb21000",
+		name:"Afonso Correia Sousa",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Segundo segredo",
 		nb: "nb21001",
+		name:"Anabela Araújo Rodrigues",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Terceiro segredo",
 		nb: "nb21002",
+		name:"André Gonçalo Ferreira",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Quarto segredo",
 		nb: "nb21003",
+		name:"Andreia Alves",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Quinto segredo",
 		nb: "nb21004",
+		name:"Catarina Oliveira",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Sexto segredo",
 		nb: "nb21005",
+		name:"Fernando Garcia",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Sétimo segredo",
 		nb: "nb21001",
+		name:"Anabela Araújo Rodrigues",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Oitavo segredo",
 		nb: "nb21001",
+		name:"Anabela Araújo Rodrigues",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Nono segredo",
 		nb: "nb21003",
+		name:"Andreia Alves",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Décimo segredo",
 		nb: "nb21004",
+		name:"Catarina Oliveira",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Décimo primeiro segredo",
 		nb: "nb21000",
+		name:"Afonso Correia Sousa",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	},
 	{
 		description: "Décimo segundo segredo",
 		nb: "nb21004",
+		name:"Catarina Oliveira",
 		challenge: "Submeter Segredo",
-		discovered: false
+		discovered: false,
+		date:new Date()
 	}]
 );
