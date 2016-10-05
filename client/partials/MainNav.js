232603e8c3facb121b5c067caa3362059e05f325
kroilon <bojan@ua.pt>
2016-10-05 20:54:20 +0100

    added icons to navigation panel

--- client/partials/MainNav.js
+++ client/partials/MainNav.js
@@ -1,11 +1,16 @@
 Template.MainNav.helpers({
 	isLoggedIn() {
 		return Session.get("loggedUser")!='' && Session.get("loggedUser")!=null && Session.get("loggedUser") != undefined;
-	}
+	},
+	firstName() {
+		var selectedPlayer = Session.get("loggedUser")[0].name;
+		console.log(selectedPlayer);
+		return selectedPlayer;
+	},
 });
 
 Template.MainNav.events({
-	'click .logout': () => {		
+	'click #logout': () => {		
 		Session.set("loggedUser",'');
 	}
 })
