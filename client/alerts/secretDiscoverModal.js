685b2ddc1c079933b59be6bd67a95879d5621f71
kroilon <bojan@ua.pt>
2016-10-22 19:00:25 +0100

    Changed default button shape

--- client/alerts/secretDiscoverModal.js
+++ client/alerts/secretDiscoverModal.js
@@ -1,5 +1,5 @@
 Template.secretDiscoverModal.events({
-  'click #delete': function(e) {
+  'click #discover': function(e) {
 
     e.preventDefault();
     Meteor.call('discoverSecret', this._id, function(error, result) {
