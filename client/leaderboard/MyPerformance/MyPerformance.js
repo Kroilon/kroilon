2aa52ff24f8adb39488cda547f7c28ff3844822e
kroilon <bojan@ua.pt>
2016-10-31 01:20:55 +0000

    Added highcharts to sections: MyProfile and MyPerformance

--- client/leaderboard/MyPerformance/MyPerformance.js
+++ client/leaderboard/MyPerformance/MyPerformance.js
@@ -4,7 +4,66 @@ import { Challenges } from '/imports/api/databasedriver.js';
 import { Rooms } from '/imports/api/databasedriver.js';
 import { Badges } from '/imports/api/databasedriver.js';
 
+Template.MyPerformance.playerChart = function() {
+    return {
+    	colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+      		'#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+        chart: {
+            type: 'area',
+            spacingBottom: 30
+        },
+        title: {
+            text: null
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        },
+        xAxis: {
+            categories: ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira',
+            	'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 
+            	'Sexta-feira']
+        },
+        yAxis: {
+            title: {
+                text: 'Points'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value;
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.x + ': ' + this.y;
+            }
+        },
+        plotOptions: {
+            area: {
+                fillOpacity: 0.5
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+        	showInLegend: false,
+            name: 'Player',
+            data: [50, 400, 0, 50, 150, 0, 0, 600, 672, 0, 150, 1250]
+        }]
+    };
+};
+
 Template.MyPerformance.helpers({
+
 
 });
 
--- client/leaderboard/MyProfile/MyProfile.js
+++ client/leaderboard/MyProfile/MyProfile.js
@@ -4,7 +4,71 @@ import { Challenges } from '/imports/api/databasedriver.js';
 import { Rooms } from '/imports/api/databasedriver.js';
 import { Badges } from '/imports/api/databasedriver.js';
 
+Template.MyProfile.playerSkillsChart = function() {
+    return {
+    	colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+      		'#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+        chart: {
+            polar: true,
+            type: 'line',
 
+            //Edit chart margin
+            margin: [0, 0, 0, 0],
+             //Edit chart spacing
+	        spacingTop: 0,
+	        spacingBottom: 0,
+	        spacingLeft: 0,
+	        spacingRight: 0
+
+
+        },
+
+        title: {
+            text: null,
+            x: -80
+        },
+
+        pane: {
+            size: '80%'
+        },
+
+        xAxis: {
+            categories: ['People', 'Communication', 'Problem Solving', 'Management',
+                    'Android'],
+            tickmarkPlacement: 'on',
+            lineWidth: 0
+        },
+
+        yAxis: {
+            gridLineInterpolation: 'polygon',
+            lineWidth: 0,
+            min: 0
+        },
+
+        tooltip: {
+            shared: true,
+            pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
+        },
+
+        legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            y: 70,
+            layout: 'vertical'
+        },
+
+        credits: {
+		    enabled: false
+		},
+
+        series: [{
+        	showInLegend: false,
+            name: 'Skills',
+            data: [5, 9, 6, 9, 2],
+            pointPlacement: 'on'
+        }]
+    };
+};
 
 Template.MyProfile.helpers({
 
@@ -14,109 +78,3 @@ Template.MyProfile.events({
 
   
 });
-
-
-var Slices = new Meteor.Collection(null);
-Session.setDefault('pieChartSort','none');
-Session.setDefault('pieChartSortModifier',undefined);
-
-if(Slices.find({}).count() === 0){
-	for(i = 0; i < 5; i++)
-		Slices.insert({
-			value:Math.floor(Math.random() * 100)
-		});
-}
-
-Template.MyProfile.rendered = function(){
-	//Width and height
-	var w = 300;
-	var h = 300;
-
-	var outerRadius = w / 2;
-	var innerRadius = 0;
-	var arc = d3.svg.arc()
-					.innerRadius(innerRadius)
-					.outerRadius(outerRadius);
-	
-	var pie = d3.layout.pie()
-		.sort(null)
-		.value(function(d) {
-			return d.value;
-		});
-	
-	//Easy colors accessible via a 10-step ordinal scale
-	var color = d3.scale.category10();
-
-	//Create SVG element
-	var svg = d3.select("#playerSkillsChart")
-				.attr("width", w)
-				.attr("height", h);
-	
-	var key = function(d){ 
-		return d.data._id;
-	};
-
-	Deps.autorun(function(){
-		var modifier = {fields:{value:1}};
-		var sortModifier = Session.get('pieChartSortModifier');
-		if(sortModifier && sortModifier.sort)
-			modifier.sort = sortModifier.sort;
-		
-		var dataset = Slices.find({},modifier).fetch();
-		
-		var arcs = svg.selectAll("g.arc")
-					  .data(pie(dataset), key);
-
-		var newGroups = 
-			arcs
-				.enter()
-				.append("g")
-				.attr("class", "arc")
-				.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-		
-		//Draw arc paths
-		newGroups
-			.append("path")
-			.attr("fill", function(d, i) {
-				return color(i);
-			})
-			.attr("d", arc);
-		
-		//Labels
-		newGroups
-			.append("text")
-			.attr("transform", function(d) {
-				return "translate(" + arc.centroid(d) + ")";
-			})
-			.attr("text-anchor", "middle")
-			.text(function(d) {
-				return d.value;
-			});
-
-		arcs
-			.transition()
-			.select('path')
-			.attrTween("d", function(d) {
-				this._current = this._current || d;
-				var interpolate = d3.interpolate(this._current, d);
-				this._current = interpolate(0);
-				return function(t) {
-					return arc(interpolate(t));
-				};
-			});
-		
-		arcs
-			.transition()
-			.select('text')
-			.attr("transform", function(d) {
-				return "translate(" + arc.centroid(d) + ")";
-			})
-			.text(function(d) {
-				return d.value;
-			});
-
-		arcs
-			.exit()
-	 		.remove();
-	});
-};
