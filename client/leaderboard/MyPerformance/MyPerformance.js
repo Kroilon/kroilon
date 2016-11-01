dadda9e09bec7691ab1857d8f733e2ed397abb06
kroilon <bojan@ua.pt>
2016-11-01 12:28:40 +0000

    added new graph to MyPerformance section

--- client/leaderboard/MyPerformance/MyPerformance.js
+++ client/leaderboard/MyPerformance/MyPerformance.js
@@ -4,10 +4,81 @@ import { Challenges } from '/imports/api/databasedriver.js';
 import { Rooms } from '/imports/api/databasedriver.js';
 import { Badges } from '/imports/api/databasedriver.js';
 
+
 Template.MyPerformance.playerChart = function() {
     return {
     	colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
       		'#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+        title: {
+            text: null
+        },
+        xAxis: {
+            crosshair: {
+                color: '#ffcccc'
+            },
+            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 
+                'Friday']
+        },
+        yAxis: {
+            title: {
+                text: 'Points'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value;
+                }
+            },
+            min: 0
+        },
+        tooltip: {
+            crosshair: {
+                color: '#de4f4f'
+            },
+            shared: true,
+            valueSuffix: ' Points'
+        },
+        plotOptions: {
+            area: {
+                fillOpacity: 0.5
+            }            
+        },
+        credits: {
+            enabled: false
+        },
+        legend: {
+        },
+        series: [{
+        	showInLegend: false,
+            name: 'Player',
+            data: [
+                50, 400, 0, 50, 150, 0, 0, 600, 672, 0, 150, 1250
+            ],
+            zIndex: 1,
+            marker: {
+                fillColor: 'white',
+                lineWidth: 3,
+                lineColor: '#de4f4f'
+            }
+        }]
+    };
+};
+
+Template.MyPerformance.helpers({
+
+
+});
+
+Template.MyPerformance.events({
+
+  
+});
+
+/*
+Template.MyPerformance.playerChart = function() {
+    return {
+        colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+            '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
         chart: {
             type: 'area',
             spacingBottom: 30
@@ -27,8 +98,8 @@ Template.MyPerformance.playerChart = function() {
         },
         xAxis: {
             categories: ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira',
-            	'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 
-            	'Sexta-feira']
+                'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 
+                'Sexta-feira']
         },
         yAxis: {
             title: {
@@ -55,19 +126,10 @@ Template.MyPerformance.playerChart = function() {
             enabled: false
         },
         series: [{
-        	showInLegend: false,
+            showInLegend: false,
             name: 'Player',
             data: [50, 400, 0, 50, 150, 0, 0, 600, 672, 0, 150, 1250]
         }]
     };
 };
-
-Template.MyPerformance.helpers({
-
-
-});
-
-Template.MyPerformance.events({
-
-  
-});
+*/
