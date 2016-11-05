0c97bc3e9fb8ff73104dcb3b0744d6abb77718ce
Pedro Almeida <pedro.gabriel.almeida@novabase.pt>
2016-11-05 15:21:04 +0000

    Mensagem de erro para login incorreto

--- client/home/HomeLogin.js
+++ client/home/HomeLogin.js
@@ -3,27 +3,34 @@ import { Academy } from '/imports/api/databasedriver.js';
 import { Session } from 'meteor/session';
 
 Template.HomeLogin.events({
-   'submit form' (event)  {
+	'submit form'(event) {
 
-   	event.preventDefault();
+		event.preventDefault();
 
-    var playerNb = $("#playerNb").val();
-	var playerPass = $("#playerPass").val();
+		var playerNb = $("#playerNb").val();
+		var playerPass = $("#playerPass").val();
 
-    var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
+		var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
 
-	console.log(latestAcademy.name);
+		//console.log(latestAcademy.name);
 
-    var user = $.grep(latestAcademy.users, function(e){
-		return e.nb == playerNb;
-	});
+		var user = $.grep(latestAcademy.users, function (e) {
+			return e.nb == playerNb;
+		});
 
-	if(user[0].nb == playerNb && playerPass == user[0].password){
+		var supposedlyLoggedUser = user[0];
+		if (supposedlyLoggedUser
+			&& supposedlyLoggedUser.nb == playerNb
+			&& playerPass == supposedlyLoggedUser.password) {
+			debugger;
 
-		Session.set("loggedUser", user);
-		console.log(Session.get("loggedUser"));
-		//$('#loginPage').html('<h3>LOGIN DONE</h3>');
-		FlowRouter.go('/map');
+			Session.set("loggedUser", user);
+			console.log(Session.get("loggedUser"));
+			//$('#loginPage').html('<h3>LOGIN DONE</h3>');
+			FlowRouter.go('/map');
+		} else {
+			debugger;
+			$('#loginPageMessage').text('Username ou password inválidos.');
 		}
-  	}
+	}
 });
