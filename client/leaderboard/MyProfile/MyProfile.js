24d507222f31d0971ec8ec3d9e4abde1ec7abce9
Pedro Almeida <pedro.gabriel.almeida@novabase.pt>
2016-11-05 13:34:04 +0000

    Mostra diagrama de skills do jogador

--- client/leaderboard/MyProfile/MyProfile.js
+++ client/leaderboard/MyProfile/MyProfile.js
@@ -4,21 +4,21 @@ import { Challenges } from '/imports/api/databasedriver.js';
 import { Rooms } from '/imports/api/databasedriver.js';
 import { Badges } from '/imports/api/databasedriver.js';
 
-Template.MyProfile.playerSkillsChart = function() {
+Template.MyProfile.playerSkillsChart = function () {
     return {
-    	colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
-      		'#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+        colors: ['#de4f4f', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+            '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
         chart: {
             polar: true,
             type: 'line',
 
             //Edit chart margin
             margin: [0, 0, 0, 0],
-             //Edit chart spacing
-	        spacingTop: 0,
-	        spacingBottom: 0,
-	        spacingLeft: 0,
-	        spacingRight: 0
+            //Edit chart spacing
+            spacingTop: 0,
+            spacingBottom: 0,
+            spacingLeft: 0,
+            spacingRight: 0
 
 
         },
@@ -34,7 +34,7 @@ Template.MyProfile.playerSkillsChart = function() {
 
         xAxis: {
             categories: ['People', 'Communication', 'Problem Solving', 'Management',
-                    'Android'],
+                'Android'],
             tickmarkPlacement: 'on',
             lineWidth: 0
         },
@@ -58,11 +58,11 @@ Template.MyProfile.playerSkillsChart = function() {
         },
 
         credits: {
-		    enabled: false
-		},
+            enabled: false
+        },
 
         series: [{
-        	showInLegend: false,
+            showInLegend: false,
             name: 'Skills',
             data: [5, 9, 6, 9, 2],
             pointPlacement: 'on'
@@ -74,111 +74,123 @@ Template.MyProfile.playerSkillsChart = function() {
 
 Template.MyProfile.helpers({
 
-    tapMyBackScore()
-    {
+    tapMyBackScore() {
         var nb = Session.get("loggedUser")[0].nb;
-        var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+        var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+        var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
 
         return user[0].counter;
     },
     TeamScore() {
 
-		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-		var users = latestAcademy.users;
-		var total_users = users.length;
+        var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+        var users = latestAcademy.users;
+        var total_users = users.length;
         var total_points = 0;
 
-        $.each(users, function(index_users, value_users){
+        $.each(users, function (index_users, value_users) {
 
-			var user_points = 0;
+            var user_points = 0;
 
-            if(value_users.score != undefined)
-            {
-                $.each(value_users.score, function(index_score, value_score){
-				    user_points += value_score.points;
-			    });
+            if (value_users.score != undefined) {
+                $.each(value_users.score, function (index_score, value_score) {
+                    user_points += value_score.points;
+                });
             }
-			
-			value_users.totalScore = user_points;
-			total_points += user_points;
-           
-		});
 
-        var average_points = (total_points/total_users - 1);
-		return parseInt(average_points);
-	},
+            value_users.totalScore = user_points;
+            total_points += user_points;
 
+        });
 
-    tapSkillsPeople(){
-        var nb = Session.get("loggedUser")[0].nb;
-        console.log("MEULOG " + nb)
-         var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
-
-        return user[0].skills[0].people;
+        var average_points = (total_points / total_users - 1);
+        return parseInt(average_points);
     },
 
-    tapSkillsCommunication(){
-         var nb = Session.get("loggedUser")[0].nb;
-         var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
 
-        return user[0].skills[0].communication;
-    },
-    
-    tapSkillsProblemSolving(){
-         var nb = Session.get("loggedUser")[0].nb;
-         var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+    tapSkillsPeople,
 
-        return user[0].skills[0].problemSolving;
-    },
-    
-    tapSkillsManagement(){
-         var nb = Session.get("loggedUser")[0].nb;
-         var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+    tapSkillsCommunication,
 
-        return user[0].skills[0].management;
-    },
-    
-    tapSkillsAndroid(){
-         var nb = Session.get("loggedUser")[0].nb;
-         var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-        var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+    tapSkillsProblemSolving,
 
-        return user[0].skills[0].android;
-    },
-    
-   	score()
-  	{
+    tapSkillsManagement,
+
+    tapSkillsAndroid,
+
+    score() {
         var nb = Session.get("loggedUser")[0].nb
-		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-		var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+        var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+        var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
 
-		var user_points = 0;
+        var user_points = 0;
 
-		$.each(user[0].score, function(index, value){
-			user_points += value.points;
-		});
+        $.each(user[0].score, function (index, value) {
+            user_points += value.points;
+        });
 
-		return user_points;
-  	},
+        return user_points;
+    },
 
-    name()
-  	{
+    name() {
         var nb = Session.get("loggedUser")[0].nb
-		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-		var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
+        var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+        var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
 
         var name = user[0].name;
 
-		return name;
-  	},
+        return name;
+    },
 });
+
+/*
+Helpers came outside in order to be used inside other configurations
+like "playerSkillsChart".
+ */
+
+function tapSkillsPeople() {
+    var nb = Session.get("loggedUser")[0].nb;
+    var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+    var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+
+    return user[0].skills[0].people;
+}
+
+function tapSkillsAndroid() {
+    var nb = Session.get("loggedUser")[0].nb;
+    var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+    var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+
+    return user[0].skills[0].android;
+}
+
+function tapSkillsManagement() {
+    var nb = Session.get("loggedUser")[0].nb;
+    var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+    var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+
+    return user[0].skills[0].management;
+}
+
+function tapSkillsProblemSolving() {
+    var nb = Session.get("loggedUser")[0].nb;
+    var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+    var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+
+    return user[0].skills[0].problemSolving;
+}
+
+function tapSkillsCommunication() {
+    var nb = Session.get("loggedUser")[0].nb;
+    var latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+    var user = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+
+    return user[0].skills[0].communication;
+}
+
+
 
 Template.MyProfile.events({
 
-  
+
 });
