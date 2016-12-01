6bbf4b249e88948a3b5f292f1cfcd417b10d14b0
Pedro Almeida <pedrogabriel@protonmail.ch>
2016-12-01 01:26:07 +0000

    Documentation added to the global helper file.

--- client/performance/PerformanceGlobalHelpers.js
+++ client/performance/PerformanceGlobalHelpers.js
@@ -1,3 +1,18 @@
+/**
+ * Use this file when you want to make available a certain
+ * function to one or more templates. By using the 'registerHelper'
+ * function we are creating a global scope that every template can 
+ * access to invoke that registered function.
+ * 
+ * Do not create one single file to contain all the helpers instead
+ * create one file per "package" like we did with the "PerformanceGlobalHelpers.js"
+ * inside the package "performance".
+ * 
+ * If you want to make a certain function available to helper files (.js files)
+ * you must export them from here and import the one you want to use
+ * inside the helper file. 
+ */
+
 
 import { Academy } from '/imports/api/databasedriver.js';
 
