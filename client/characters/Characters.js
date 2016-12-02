39902efc6da166ba9a5328fa2ea0b0a700f4fa64
kroilon <bojan@ua.pt>
2016-12-02 00:26:31 +0000

    Characters section - added table with skills

--- client/characters/Characters.js
+++ client/characters/Characters.js
@@ -1,35 +1,77 @@
 import { Template } from 'meteor/templating';
 import { Academy } from '/imports/api/databasedriver.js';
 
-var ItemsPerPage = 1;
-
-Template.Characters.events({
-	'click h1': () => {
-		var page = FlowRouter.getParam("page");
-		console.log(page);
-	}
-})
-
 Template.Characters.helpers({
-	characters() {
-		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
+    user(nb) {
 
-		return latestAcademy.users;
-	},
-	score(nb) {
+      var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
 
-		var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
-		var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
-		var user_points = 0;
+      var user = $.grep(latestAcademy.users, function(e){ return e.nb == nb; });
 
-		$.each(user[0].score, function(index, value){
-			user_points += value.points;
-		});
+      return user[0];
+    },
+    academyPlayers: function () {
 
-		return user_points;
-	},
-	isAdmin: function (name) {
-	    return name !== "Admin"
-	}
-}); 
+        var latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
+        var users = latestAcademy.users;
+        users.splice(0, 1);
+        return users;
+    },
+    tableSettings : function () {
+      return {
+        showFilter: false,
+        rowsPerPage: 10,
+        showNavigation: 'auto',
+        showRowCount: true,
+        rowsPerPage: 10,
+        showNavigationRowsPerPage: true,
+        showColumnToggles: true,
+        fields: [
+          {
+            key: 'name',
+            label: 'Name',
+            headerClass: 'col-md-2',
+            sortDirection: 'ascending',
+            hidden: false
+          },
+          {
+            key: 'skills.0.people',
+            label: 'People',
+            headerClass: 'col-md-2',
+            sortDirection: 'descending',
+            hidden: false
+          },
+          {
+            key: 'skills.0.communication',
+            label: 'Communication',
+            headerClass: 'col-md-2',
+            sortDirection: 'descending',
+            hidden: false
+          },
+          {
+            key: 'skills.0.problemSolving',
+            label: 'Problem solving',
+            headerClass: 'col-md-2',
+            sortDirection: 'descending',
+            hidden: false
+          },
+          {
+            key: 'skills.0.management',
+            label: 'Management',
+            headerClass: 'col-md-2',
+            sortDirection: 'descending',
+            hidden: false
+          },
+          {
+            key: 'skills.0.android',
+            label: 'Android',
+            headerClass: 'col-md-2',
+            sortDirection: 'descending',
+            hidden: false
+          }
+        ]
+      };
+    },
+
+});
 
