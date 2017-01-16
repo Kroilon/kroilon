928e77ab5c3a356e8aa505a90aaf8734c8e65e0a
kroilon <bojan@ua.pt>
2017-01-16 23:58:39 +0000

    Management secrets - modified due to the previous changes

--- client/alerts/secretDiscoverModal.js
+++ client/alerts/secretDiscoverModal.js
@@ -1,8 +1,33 @@
+import { Template } from 'meteor/templating';
+import { Academy } from '/imports/api/databasedriver.js';
+import { Secrets } from '/imports/api/databasedriver.js';
+
 Template.secretDiscoverModal.events({
   'click #discover': function(e) {
 
     e.preventDefault();
+
+    let secretNB = this.nb;
+    //let player = getUserByNB(secretNB);
+    let latestAcademy = Academy.findOne({}, {sort: {date: -1, limit: 1}});
+    let dataSecretInsert = {};
+
     Meteor.call('discoverSecret', this._id, function(error, result) {
+      if (error) {
+        alert(error);
+      } 
+    });
+
+    dataSecretInsert = 
+    {
+      date: new Date(),
+      countType: "ACTIVITY",
+      name: "Inserir segredo",
+      points: 200,
+      pointsType: "HP"
+    };
+
+    Meteor.call('updateScore', latestAcademy._id, secretNB, dataSecretInsert, function(error, result) {
       if (error) {
         alert(error);
       } 
@@ -11,3 +36,9 @@ Template.secretDiscoverModal.events({
     Modal.hide('secretDiscoverModal');
   }
 });
+
+function getUserByNB(nb) {
+  let latestAcademy = Academy.findOne({}, { sort: { date: -1, limit: 1 } });
+  let users = $.grep(latestAcademy.users, function (e) { return e.nb == nb; });
+  return users[0];
+}
