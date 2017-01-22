f7cc45fd46eed4ce5503cfdb1437cc7f6f299c00
kroilon <bojan@ua.pt>
2017-01-22 21:40:06 +0000

    Various css improvements

--- client/management/activities/tableActivity/TableActivityInfo.js
+++ client/management/activities/tableActivity/TableActivityInfo.js
@@ -57,7 +57,7 @@ Template.TableActivityInfo.helpers({
       {
         $.each(scores, function(idx_scores, val_scores)
         {
-          points.push({"player":val_players.name, "nb":val_players.nb, "name":val_scores.name, "points":val_scores.points + " " + val_scores.pointsType, "date":val_scores.date});
+          points.push({"player":val_players.name, "nb":val_players.nb, "type":val_scores.countType, "name":val_scores.name, "points":val_scores.points + " " + val_scores.pointsType, "date":val_scores.date});
         //  console.log(points_a);
         //  console.log(points[0]);
         });
