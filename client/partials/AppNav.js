f587f7a7bd52896927d7eb7e184f9869c1957189
kroilon <bojan@ua.pt>
2017-01-29 22:23:50 +0000

    CSS upagrades from Mohamed

--- client/partials/AppNav.js
+++ client/partials/AppNav.js
@@ -8,4 +8,38 @@ Template.MainNav.events({
 	'click .logout': () => {
 		Session.set("loggedUser", '');
 	}
-})
+});
+
+    
+	/*Menu-toggle*/
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+        alert(1);
+    });
+
+    function htmlbodyHeightUpdate(){
+		var height3 = $( window ).height()
+		var height1 = $('.nav').height()+50
+		height2 = $('.main').height()
+		if(height2 > height3){
+			$('html').height(Math.max(height1,height3,height2)+10);
+			$('body').height(Math.max(height1,height3,height2)+10);
+		}
+		else
+		{
+			$('html').height(Math.max(height1,height3,height2));
+			$('body').height(Math.max(height1,height3,height2));
+		}
+		
+	}
+	$(document).ready(function () {
+		htmlbodyHeightUpdate()
+		$( window ).resize(function() {
+			htmlbodyHeightUpdate()
+		});
+		$( window ).scroll(function() {
+			height2 = $('.main').height()
+  			htmlbodyHeightUpdate()
+		});
+	});
